b02d7419c9dec24cacd6861277e1de281bcf2ce2
Felicia Himawan <ffeliciah.00@gmail.com>
2024-07-15 09:54:51 +0700

    Create and Edit Plan

---
+++ NC2/Our App/Presentation/Edit Plan/EditPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPlanView.swift
+//  NC2
+//
+//  Created by Felicia Himawan on 14/07/24.
+//
+
+import SwiftUI
+
+struct EditPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPlanView()
+}
---
+++ NC2/Our App/Presentation/Edit Plan/EditPlanViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EditPlanViewModel.swift
+//  NC2
+//
+//  Created by Felicia Himawan on 14/07/24.
+//
+
+import Foundation
