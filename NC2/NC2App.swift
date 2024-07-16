d408a99e3e601cc0f7443561d0a4ac605a450c7e
sabaach <safrieb3@gmail.com>
2024-07-16 10:47:07 +0700

    +Update Notification

--- NC2/NC2App.swift
+++ NC2/NC2App.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import UserNotifications
 
 @main
 struct NC2App: App {
@@ -18,6 +19,7 @@ struct NC2App: App {
         } catch {
             fatalError("Failed to initialize SwiftData")
         }
+        ReminderViewModel.shared.requestAuthorization()
     }
     
     var body: some Scene {
