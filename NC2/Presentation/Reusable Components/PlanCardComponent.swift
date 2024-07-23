4a5c747ecc7ff5ee0551884a4aee72ddfa58ea4a
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-07-23 10:41:52 +0700

    Fix bugs layout card homepage

--- NC2/Presentation/Reusable Components/PlanCardComponent.swift
+++ NC2/Presentation/Reusable Components/PlanCardComponent.swift
@@ -10,64 +10,60 @@ import SwiftUI
 struct PlanCardComponent: View {
     var plan: HomeCardUIModel
     @EnvironmentObject var dependencyInjection: DependencyInjection
-    
+
     var body: some View {
         NavigationLink(destination: DetailPlanView(planId: plan.id)
             .environmentObject(dependencyInjection.detailPlanViewModel())
         ) {
-            ZStack {
+            VStack(alignment: .leading, spacing: 8) {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(plan.title)
+                            .shadowedText(font: .body)
+
+                        Text(plan.location.nameLocation.truncated(to: 15))
+                            .shadowedText(font: .subheadline)
+                    }
+
+                    Spacer()
+
+                    if plan.allDay {
+                        Text("All Day")
+                            .shadowedText(font: .subheadline)
+                    } else {
+                        VStack(alignment: .trailing, spacing: 8) {
+                            Text(plan.durationPlan.start.formattedTime())
+                                .shadowedText(font: .subheadline)
+
+                            Text(plan.durationPlan.end.formattedTime())
+                                .shadowedText(font: .subheadline)
+                        }
+                    }
+                }
+
+                HStack {
+                    Image(systemName: "cloud.sun.fill")
+                        .shadowedText(font: .body)
+
+                    Text("\(String(format: "%.1f", plan.temperature ?? 0))°C")
+                        .shadowedText(font: .body)
+
+                    Text(plan.weatherDescription?.toFrontCapital() ?? "fetching data")
+                        .shadowedText(font: .body)
+                }
+                .padding(.vertical, 4)
+            }
+//            .frame(width: 100)
+            .padding(.horizontal, 16)
+            .padding(.vertical, 8)
+            .background(
                 Image(plan.backgroundCard ?? "clearCard")
                     .resizable()
                     .scaledToFill()
                     .clipShape(RoundedRectangle(cornerRadius: 10))
-                
-                VStack(alignment: .leading, spacing: 8) {
-                    HStack(alignment: .top) {
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text(plan.title)
-                                .shadowedText(font: .body)
-                            
-                            Text(plan.location.nameLocation)
-                                .shadowedText(font: .subheadline)
-                        }
-                        
-                        Spacer()
-                        
-                        if plan.allDay {
-                            Text("All Day")
-                                .shadowedText(font: .subheadline)
-                        } else {
-                            VStack(alignment: .trailing, spacing: 8) {
-                                Text(plan.durationPlan.start.formattedTime())
-                                    .shadowedText(font: .subheadline)
-                                
-                                Text(plan.durationPlan.end.formattedTime())
-                                    .shadowedText(font: .subheadline)
-                            }
-                        }
-                    }
-                    
-                    HStack {
-                        Image(systemName: "cloud.sun.fill")
-                            .shadowedText(font: .body)
-                        
-                        Text("\(String(format: "%.1f", plan.temperature ?? 0))°C")
-                            .shadowedText(font: .body)
-                        
-                        Text(plan.weatherDescription?.toFrontCapital() ?? "fetching data")
-                            .shadowedText(font: .body)
-                    }
-                    .padding(.vertical, 4)
-                }
-                .padding(.horizontal, 16)
-                .padding(.vertical, 8)
-            }
-            .padding(.vertical, 4)
+            )
             .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(.vertical, 4)
         }
     }
-}
-
-#Preview {
-    PlanCardComponent(plan: dummyPlansEntity[6])
 }
